1fd0e75374ffc7e2ed22d205e36c6c02d079e066
leander006 <leanderdsilva06@gmail.com>
2022-10-18 04:12:04 +0530

    stagging:beforeEach

--- test/stagging/Voting.stagging.test.js
+++ test/stagging/Voting.stagging.test.js
@@ -7,4 +7,13 @@ const {
 const { expect, assert } = require("chai");
 developmentChains.includes(network.name)
   ? describe.skip
-  : describe("Voting stagging test", function () {});
+  : describe("Voting stagging test", function () {
+    let voting, deployer, interval;
+    const chainId = network.config.chainId;
+    beforeEach(async function () {
+      deployer = (await getNamedAccounts()).deployer;
+      await deployments.fixture(["all"]);
+      voting = await ethers.getContract("Voting", deployer);
+      interval = await voting.getInterval();
+    });
+  });
